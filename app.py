a4f0e599469667bc5fbc83971179612abfb1a8fc
MuhamadRizalpermana2205101082 <123rizalpermana97@gmail.com>
2025-09-19 18:35:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-import streamlit as st
+import gradio as gr
 import tensorflow as tf
-from PIL import Image
 import numpy as np
-
-st.title("🌿 Deteksi Daun Tanaman Hias")
+from PIL import Image
 
 # Load model
-model = tf.keras.models.load_model("model_daun.h5")
+model = tf.keras.models.load_model("mymodel(1).h5")
 
-uploaded_file = st.file_uploader("Upload gambar daun", type=["jpg", "png", "jpeg"])
+# Label kelas daun (ganti sesuai datasetmu)
+label_kelas = ["Daun Hijau", "Daun Merah Hijau", "Daun Lainnya"]
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file).resize((128, 128))  # sesuaikan dengan input model
-    st.image(image, caption="Gambar diupload", use_column_width=True)
-
+def prediksi_daun(img):
+    image = img.resize((128, 128))
     img_array = np.array(image) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
     pred = model.predict(img_array)
     kelas = np.argmax(pred, axis=1)[0]
+    return f"Hasil Prediksi: {label_kelas[kelas]}"
 
-    st.write(f"Hasil Prediksi: **{kelas}**")
+# Interface Gradio
+demo = gr.Interface(fn=prediksi_daun, inputs=gr.Image(type="pil"), outputs="text", title="🌿 Deteksi Daun Tanaman Hias")
+demo.launch()
+
+
